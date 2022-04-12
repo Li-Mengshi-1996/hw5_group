6204bf284b599e3ec0e390fae55acc7d95fd3735
cdpxhjs <jhuang24@student.gsu.edu>
2022-04-12 17:11:57 -0400

    add first replica cdn

--- mapping.py
+++ mapping.py
@@ -1,5 +1,14 @@
 from cdn_info import MAPS
 
 UNKNOWN = 'Unknown'
+CDN_MAPPINGS = [
+    {
+        'hostname':'p5-http-a.5700.network',
+        'ip_address':'50.116.41.109',
+        'region':'US-Georgia',
+        'latitude':'33.798458099365234',
+        'longitude':'-84.3882827758789'
+    }
+]
 def get_nearest_cdn(source_ip):
     return MAPS[0]['ip']
