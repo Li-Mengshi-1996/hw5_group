0916911b4f633aac3ba2c39033ed65c0773842da
cdpxhjs <jhuang24@student.gsu.edu>
2022-04-18 22:00:41 -0400

    add geolocation

--- mapping.py
+++ mapping.py
@@ -1,4 +1,8 @@
 from cdn_info import MAPS
+from ipaddress import ip_network,ip_address
+import os
+import json
+import math
 
 UNKNOWN = 'Unknown'
 CDN_MAPPINGS = [
@@ -10,5 +14,42 @@ CDN_MAPPINGS = [
         'longitude':'-84.3882827758789'
     }
 ]
+
+def get_geoLocation(ip):
+    source_ip2=str.encode(ip)
+    try:
+        output_stream=os.popen('curl -u "708079:xYVsrhhTQiHs9b0M" "https://geolite.info/geoip/v2.1/city/'+source_ip2+'?pretty"')
+        json_str=json.loads(output_stream.read().strip())
+        latitude= (json_str['location']['latitude'])
+        longitude=(json_str['location']['longitude'])
+        return latitude, longitude
+    except Exception as e:
+        return None, None
+
+def get_geo_distance(lat1,lon1,lat2,lon2):
+    R = 6373.0#rad of earth
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    #Haversine formula
+    a = math.sin(dlat/2)**2+math.cos(lat1)*math.cos(lat2)*math.sin(dlon/2)**2
+    c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+    return R*c
+    
 def get_nearest_cdn(source_ip):
-    return MAPS[0]['ip']
+    latitude,longitude = get_geoLocation(source_ip)
+    if latitude is None or longitude is None:
+        return CDN_MAPPINGS[0]['ip_address']
+    best_dist=None
+    best_cdn=None
+    for c in CDN_MAPPINGS:
+        dist = get_geo_distance(latitude,longitude,c['latitude'],c['longitude'])
+        if not best_dist or best_dist > dist:
+            best_dist = dist
+            best_cdn = c['ip_address']
+    return best_cdn
+
+get_nearest_cdn("")
