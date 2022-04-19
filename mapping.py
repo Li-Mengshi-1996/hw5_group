14e298fe1f80ecd800a1551f5a7d6a865550b69e
cdpxhjs <jhuang24@student.gsu.edu>
2022-04-18 22:12:17 -0400

    add comment

--- mapping.py
+++ mapping.py
@@ -16,6 +16,12 @@ CDN_MAPPINGS = [
 ]
 
 def get_geoLocation(ip):
+    """
+    It takes an IP address as input, and returns the latitude and longitude of the IP address
+    
+    :param ip: The IP address of the source
+    :return: The latitude and longitude of the IP address.
+    """
     source_ip2=str.encode(ip)
     try:
         output_stream=os.popen('curl -u "708079:xYVsrhhTQiHs9b0M" "https://geolite.info/geoip/v2.1/city/'+source_ip2+'?pretty"')
@@ -27,6 +33,16 @@ def get_geoLocation(ip):
         return None, None
 
 def get_geo_distance(lat1,lon1,lat2,lon2):
+    """
+    It takes the latitude and longitude of two points on the Earth, and returns the distance between
+    them in kilometers
+    
+    :param lat1: latitude of the first point
+    :param lon1: longitude of the first point
+    :param lat2: latitude of the second point
+    :param lon2: longitude of the second point
+    :return: The distance between two points on the earth.
+    """
     R = 6373.0#rad of earth
     lat1 = math.radians(lat1)
     lon1 = math.radians(lon1)
@@ -40,6 +56,13 @@ def get_geo_distance(lat1,lon1,lat2,lon2):
     return R*c
     
 def get_nearest_cdn(source_ip):
+    """
+    Given a source IP address, find the nearest CDN server by calculating the distance between the
+    source IP address and the CDN server
+    
+    :param source_ip: The IP address of the client requesting the content
+    :return: The IP address of the nearest CDN
+    """
     latitude,longitude = get_geoLocation(source_ip)
     if latitude is None or longitude is None:
         return CDN_MAPPINGS[0]['ip_address']
@@ -52,4 +75,5 @@ def get_nearest_cdn(source_ip):
             best_cdn = c['ip_address']
     return best_cdn
 
-get_nearest_cdn("")
+result = get_nearest_cdn("173.76.190.67")
+print (result)
